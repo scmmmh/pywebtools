7d39645d74d93d3fc9390e8f6003c578c5db31ff
Mark <mark.hall@work.room3b.eu>
2012-11-07 19:35:19 +0000

    Cleanup.

--- src/pywebtools/text.py
+++ src/pywebtools/text.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-'''Copyright 2012 Mark Hall (Mark.Hall@work.room3b.eu)
-
-This file is part of the PyWebTools.
-
-The PyWebTools is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The PyWebTools are distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with PyWebTools. If not, see <http://www.gnu.org/licenses/>.
-'''
+# Copyright 2012 Mark Hall (Mark.Hall@work.room3b.eu)
+# 
+# This file is part of the PyWebTools.
+# 
+# The PyWebTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# The PyWebTools are distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PyWebTools. If not, see <http://www.gnu.org/licenses/>.
 
 def title(string):
     if len(string) > 0:
