932a0874d4aca78b3ad286162cfc4bcd8d8160de
Mark <mark.hall@work.room3b.eu>
2012-11-13 21:00:31 +0000

    Add a render decorator that handles different content types.

---
+++ src/pywebtools/renderer/__init__.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Mark Hall (Mark.Hall@work.room3b.eu)
+# 
+# This file is part of the PyWebTools.
+# 
+# The PyWebTools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# The PyWebTools are distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PyWebTools. If not, see <http://www.gnu.org/licenses/>.
+
+u"""
+:mod:`pyramid_tools.renderer` -- Genshi renderer for Pyramid
+============================================================
+
+This packages provides the :func:`~pywebtools.renderer.renderer` decorator
+that provides a flexible rendering intermediary for the Pyramid framework.
+
+IMPORTANT! The required Python packages for this module are not installed
+automatically. You must install the following::
+
+  ``pip install mimeparse decorator pyramid``
+
+to be able to use this module.
+
+.. moduleauthor:: Mark Hall <mark.hall@mail.room3b.eu>
+"""
+
+import csv
+import json
+import mimeparse
+
+from copy import copy
+from decorator import decorator
+from genshi.template import TemplateLoader, loader
+from pyramid.httpexceptions import HTTPNotAcceptable
+from pyramid.request import Request
+from pyramid.response import Response
+from StringIO import StringIO
+
+from .htmlfiller import HTMLFormFiller
+
+_genshi_loader = None
+_template_defaults = {}
+
+class RendererException(Exception):
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return self.value
+
+def init(settings, template_defaults=None):
+    global _genshi_loader, _template_defaults
+    if 'genshi.template_path' not in settings:
+        raise RendererException('genshi.template_path not set in the configuration')
+    if template_defaults:
+        _template_defaults = template_defaults
+    auto_reload = ('pyramid.reload_templates' in settings and settings['pyramid.reload_templates'] == 'true')
+    template_paths = []
+    for template_path in [tp for line in settings['genshi.template_path'].strip().split('\n') for tp in line.split(',')]:
+        if template_path.strip() != '':
+            if ':' in template_path:
+                template_paths.append(loader.package(template_path[0:template_path.find(':')], template_path[template_path.find(':') + 1:]))
+            else:
+                template_paths.append(template_path)
+    _genshi_loader = TemplateLoader(template_paths,
+                                    auto_reload=auto_reload)
+
+def request_from_args(*args):
+    if len(args) == 1 and isinstance(args[0], Request):
+        return args[0]
+    elif len(args) == 2 and isinstance(args[1], Request):
+        return args[1]
+    else:
+        raise RendererException('No request found')
+    
+def template_defaults(request):
+    global _template_defaults
+    template_defaults = copy(_template_defaults)
+    template_defaults['r'] = request
+    return template_defaults
+
+def match_response_type(view_content_types, request):
+    accept_header = unicode(request.accept)
+    if request.matchdict and 'ext' in request.matchdict and request.matchdict['ext']:
+        if request.matchdict['ext'] == 'html':
+            accept_header = 'text/html'
+        elif request.matchdict['ext'] == 'json':
+            accept_header = 'application/json'
+        elif request.matchdict['ext'] == 'csv':
+            accept_header = 'text/csv'
+        elif request.matchdict['ext'] == 'xml':
+            accept_header = 'application/xml'
+    response_type = mimeparse.best_match(view_content_types.keys(), accept_header)
+    if not response_type or response_type not in view_content_types:
+        raise HTTPNotAcceptable()
+    return response_type
+
+def handle_html_response(request, response_template, result):
+    template = _genshi_loader.load(response_template)
+    if 'e' in result:
+        template = template.generate(**result) | HTMLFormFiller(data=result['e'].params)
+    else:
+        result['e'] = None
+        template = template.generate(**result)
+    response = Response(template.render('xhtml'))
+    return response
+
+def handle_json_response(request, result):
+    del result['h']
+    del result['r']
+    if 'e' in result:
+        result['e'] = result['e'].error_dict
+    response = Response(json.dumps(result))
+    return response
+
+def handle_csv_response(request, result):
+    f = StringIO()
+    writer = csv.DictWriter(f, result['columns'])
+    writer.writeheader()
+    for row in result['rows']:
+        writer.writerow(row)
+    response = Response(unicode(f.getvalue()))
+    f.close()
+    return response
+
+def handle_xml_response(request, response_template, result):
+    template = _genshi_loader.load(response_template)
+    template = template.generate(**result)
+    response = Response(template.render('xml'))
+    return response
+
+def render(content_types={}, allow_cache=True):
+    def wrapper(f, *args, **kwargs):
+        request = request_from_args(*args)
+        result = template_defaults(request)
+        result.update(f(*args, **kwargs))
+        response_type = match_response_type(content_types, request)
+        response_template = content_types[response_type]
+        if response_type == 'application/json':
+            response = handle_json_response(request, result)
+            response.cache_control = 'no-cache'
+            response.pragma = 'no-cache'
+            response.expires = '0'
+        elif response_type == 'text/html':
+            response = handle_html_response(request, response_template, result)
+        elif response_type == 'text/csv':
+            response = handle_csv_response(request, result)
+        elif response_type == 'application/xml':
+            response = handle_xml_response(request, response_template, result)
+        response.content_type = response_type
+        request.response.merge_cookies(response)
+        if not allow_cache:
+            response.cache_control = 'no-cache'
+            response.pragma = 'no-cache'
+            response.expires = '0'
+        return response
+    return decorator(wrapper)
