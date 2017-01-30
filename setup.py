326b42a1ba01c530d893ab9adacd784210d09d14
Mark <Mark.Hall@edgehill.ac.uk>
2017-01-30 15:08:33 +0000

    Verison 1.1.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ tests_require = [
     ]
 
 setup(name='PyWebTools',
-      version='1.1.2',
+      version='1.1.3',
       description='A collection of helpers for use with Pyramid, Kajiki, Formencode, and SQLAlchemy',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
